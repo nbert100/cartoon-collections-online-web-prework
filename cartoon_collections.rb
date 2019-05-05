c535c1deef1fb50e185650351db8db887df25152
Nia Bert <nbert14@gmail.com>
2019-05-05 02:35:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def summon_captain_planet(array)
   array.collect { |string| string.capitalize + "!" }
 end
 
-def long_planeteer_calls(array)# code an argument here
+def long_planeteer_calls(array)
+ 
   # Your code here
 end
 
