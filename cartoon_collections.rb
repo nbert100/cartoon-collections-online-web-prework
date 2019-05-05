ac4be1ae17dbf11e73cef4f4318f20365c85b58d
Nia Bert <nbert14@gmail.com>
2019-05-05 03:17:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 
 def find_the_cheese(array)# code an argument here
    cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find(ifnone = nil) {|string| array.include? (cheese_types) }
+  array.find(ifnone = nil) {|string| cheese_types.include? (array) }
    
 end
