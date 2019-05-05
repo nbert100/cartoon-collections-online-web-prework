64a6c0fc44233b1348bd650c7b5dccd40c7ca4d1
Nia Bert <nbert14@gmail.com>
2019-05-05 02:52:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ def find_the_cheese(array)# code an argument here
  
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |string|
-    string.cheese_types
+    string.cheese_types?
 end
